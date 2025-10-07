28a077ec5efe7662d13cb16992a09cc3038a5987
DhanHaidar <dhanhaidar56@gmail.com>
2025-10-07 22:19:27 +0700

    initial commit to new repo

--- Kalijaga-EventHub-copy1-/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// routes/web.php
-Route::get('/{any}', function () {
-    return view('app'); // app.blade.php menampung root Vue
-})->where('any', '.*');
-
